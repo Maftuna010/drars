f63e19cf642e7dcc7bcdba34b923d5390d9522fc
Maftuna010 <niyozmetovmuzaffar11@gmail.com>
2026-05-23 15:57:20 +0500

    ghjghgbh
    hjghjghjghj

---
+++ dars.py
@@ -0,0 +1,22 @@
+class Davlatlar:
+   def__init_-(self,nomi,poytaxti,aholisi):
+   self.nomi = nomi
+   self.poytaxti = aholisi
+   self.joylashuvi = "Yevropa"
+   self.tili = "Ingliz tili"
+   self.maydoni = 1000000
+ def get_nomi(self):
+    return f"{self.nomi.title()} davlati"
+
+def get_poytaxti(self):
+    return f"{self.nomi.title()}ning poytaxti {self.poytaxti.title()} shahridir."
+def get_aholisi(self):
+    return f"{self.nomi.title()}ning aholisi {self.aholisi}mln kishidir."
+def get_joylashuvi(self):
+    return f"{self.nomi.title()} davlati {self.joylashuvi.title()} da joylashgan."
+
+
+
+
+
+    
